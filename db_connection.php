5f98ed0acc9e5da72ba45dcb76eeef790026a5c3
Hridoy <154232641+1dev-hridoy@users.noreply.github.com>
2024-04-28 12:27:00 +0600

    db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "fdb1034.awardspace.net";
-$username = "4477624_hridoy";
-$password = "1#x}U}3)7a#z1gix";
-$dbname = "4477624_hridoy";
+$servername = "sever_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_db_name";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
